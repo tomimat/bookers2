2c7295b04dc1cf1e4e6353a0098c3bf195a4f9e8
EC2 Default User <ec2-user@ip-172-31-27-51.ap-southeast-2.compute.internal>
2024-06-11 08:12:31 +0000

    create error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,18 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to 'show'
+     if @book.save
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to book_path(@book.id)
+    else
+      flash[:notice] = "投稿に失敗しました。"
+        render :new
+    end
   end
 
 
@@ -15,6 +21,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
    private

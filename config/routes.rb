f3c2d83eb1d97a93fd1e2cf571f4b282cb023c2f
EC2 Default User <ec2-user@ip-172-31-27-51.ap-southeast-2.compute.internal>
2024-06-16 02:37:38 +0000

    bookers finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/top' => 'homes#top'
+  root to: 'homes#top'
    patch 'books/:id' => 'books#update', as: 'update_book'
    delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
